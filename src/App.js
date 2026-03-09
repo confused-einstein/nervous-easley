def086495ea43279fba0021c6c487b0bb578eb2e
himanshu soni <43922410+confused-einstein@users.noreply.github.com>
2026-03-09 21:24:28 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <h3> Hello fuckers</h3>
+      </div>
+      <nav>
+        <li>Home</li>
+        <li>contact us</li>
+      </nav>
+    </>
   );
 }
