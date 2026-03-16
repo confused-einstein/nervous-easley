aa09b2c72675c43830793c4766256b811f78e12e
himanshu soni <43922410+confused-einstein@users.noreply.github.com>
2026-03-16 19:42:08 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./styles.css";
+import Top2Box from "./Components/NewComp";
 
 export default function App() {
   return (
     <>
+      <Top2Box name="Himanshu Sondi" />
       <div className="App">
         <h1>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
---
+++ src/Components/NewComp.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+import "./CompStyle.css";
+export default function Top2Box(poop) {
+  return (
+    <div>
+      <h1 className="FirstName">Hello ji Sasriayakal</h1>
+      <h1 className="FirstName">{poop.name}</h1>
+    </div>
+  );
+}
+
+// Top2Box.PropTypes = PropTypes.string;
